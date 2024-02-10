5c25c0ced881673edb3deddc9bce4c9168d57e95
AKEEL LASHLEY <akeellashley@gmail.com>
2024-02-10 16:34:44 -0500

    update auth routes for google sign on

--- src/config/strategies/authRoutes.ts
+++ src/config/strategies/authRoutes.ts
@@ -10,10 +10,37 @@ router.get(
 
 router.get(
   '/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  (req, res) => {
-    res.redirect('/')
-  },
+  passport.authenticate('google', {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: '/login/failed',
+  }),
 )
+router.get('/login/failed', (req, res) => {
+  res.status(401).json({
+    error: true,
+    message: 'Log in failure',
+  })
+})
+
+router.get('/login/success', (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      message: 'Successfully Logged In',
+      user: req.user,
+    })
+  } else {
+    res.status(403).json({ error: true, message: 'Not Authorized' })
+  }
+})
+
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return res.status(500).send('Failed to log out')
+    }
+    res.redirect(`${process.env.CLIENT_URL}/login`)
+  })
+})
 
 export default router
